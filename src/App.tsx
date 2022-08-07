9e9b9803257fc88aa18a90f32a5de57d048ec163
chidiebereojingwa <ojingwaoji@gmail.com>
2022-08-07 16:41:36 +0100

    Add Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import './App.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
  
   return (
     <div className="App">
       <h1>GitHub Viewer</h1>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/repositories">Repositories</Link>
+            </li>
+            <li>
+              <Link to="/followers">Followers</Link>
+            </li>
+            <li>
+              <Link to="/gists">Gist</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <switch>
+            <Route exact path="/">
+              <h1>Home</h1>
+            </Route>
+            <Route path="/repositories">
+              <h1>Repositories</h1>
+            </Route>
+            <Route path="/followers">
+              <h1>Followers</h1>
+            </Route>
+            <Route path="/gists">
+              <h1>Gist</h1>
+            </Route>
+          </switch>
+        </main>
+      </Router>
     </div>
   );
 }
