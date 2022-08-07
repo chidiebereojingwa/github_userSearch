bde6fa61c0e2d9390fa05ba9f10521e18cd44281
chidiebereojingwa <ojingwaoji@gmail.com>
2022-08-07 14:58:03 +0100

    c

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1>GitHub Viewer</h1>
+    </div>
+  );
+}
+
+export default App;
